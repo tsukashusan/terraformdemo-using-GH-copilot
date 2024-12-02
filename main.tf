129b53863db86480cad88b853ea5334b751bb353
tsukabon <shtsukam@outlook.com>
2024-12-02 09:19:31 +0900

    frist commit.

---
+++ main.tf
@@ -0,0 +1,109 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.1.2"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+resource "azurerm_network_security_group" "example" {
+    name                = "example-nsg"
+    location            = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+
+    security_rule {
+        name                       = "AllowAzure"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "AzureLoadBalancer"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "AllowSpecificIP"
+        priority                   = 200
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "61.207.80.134"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+    subnet_id                 = azurerm_subnet.example.id
+    network_security_group_id = azurerm_network_security_group.example.id
+}
+resource "azurerm_resource_group" "example" {
+    name     = "example-resources"
+    location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+    name                = "example-network"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+    name                 = "internal"
+    resource_group_name  = azurerm_resource_group.example.name
+    virtual_network_name = azurerm_virtual_network.example.name
+    address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+    name                = "example-nic"
+    location            = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+
+    ip_configuration {
+        name                          = "internal"
+        subnet_id                     = azurerm_subnet.example.id
+        private_ip_address_allocation = "Dynamic"
+    }
+    enable_accelerated_networking = true
+}
+
+resource "azurerm_virtual_machine" "example" {
+    name                  = "example-machine"
+    location              = azurerm_resource_group.example.location
+    resource_group_name   = azurerm_resource_group.example.name
+    network_interface_ids = [azurerm_network_interface.example.id]
+    vm_size               = "Standard_D4s_v3"
+
+    storage_os_disk {
+        name              = "example-os-disk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "0001-com-ubuntu-server-focal"
+        sku       = "20_04-lts-gen2"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "hostname"
+        admin_username = "adminuser"
+        admin_password = "Password1234!"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+}
